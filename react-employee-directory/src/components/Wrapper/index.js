75a4523e2fdfe99551a3b3264be5bf36d9ac8f5f
Kerem Karaman <keremukaraman@gmail.com>
2020-12-08 09:19:47 -0600

    Updates

--- react-employee-directory/src/components/Wrapper/index.js
+++
@@ -1,9 +0,0 @@
-
-import "./style.css";
-
-function Wrapper(props) {
-  // children for the wrapper component is the title and 3 FriendCards
-  return <div className="wrapper">{props.children}</div>;
-}
-
-export default Wrapper;
